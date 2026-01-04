b332e43be8edcb7c1d4fce0069bcb5a0e3ddab7b
James (Shoti) <163653982+James43233@users.noreply.github.com>
2026-01-04 22:25:10 +0800

    chore: organized files for deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,27 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
 
-type PrismaClientInstance = InstanceType<typeof PrismaClient>;
+type PrismaClientInstance = InstanceType<typeof PrismaClient>
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClientInstance;
-  pgPool?: Pool;
-};
+  prisma?: PrismaClientInstance
+  pgPool?: Pool
+}
 
 const pgPool =
   globalForPrisma.pgPool ??
   new Pool({
     connectionString: process.env.DATABASE_URL,
-  });
+  })
 
-const adapter = new PrismaPg(pgPool);
+const adapter = new PrismaPg(pgPool)
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.pgPool = pgPool;
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.pgPool = pgPool
+  globalForPrisma.prisma = prisma
 }
