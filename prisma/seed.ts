62ae9adae22424ebd13fb2293a1116366f70d646
James (Shoti) <163653982+James43233@users.noreply.github.com>
2026-01-04 22:30:19 +0800

    ci: database trouble fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaClient, Prisma } from "../generated/prisma/client";
 import 'dotenv/config';
 
 const prisma = new PrismaClient();
